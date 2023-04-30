9ea11582e00e9987e276788c289f837f4e053a0e
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-04-30 19:04:46 +0000

    feat: barebones of hyperleaflet demo

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,27 @@
-const importCode = document.getElementById("import-code")
-const originalHTML = 
-`<!-- import leaflet -->
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js">
-/>
-<!-- import hyperleaflet -->
-<script defer src="https://www.unpkg.com/hyperleaflet@0.2.3"></script>
-`
-const html = hljs.highlightAuto(originalHTML).value
-console.log(html)
-importCode.innerHTML = html
+const hyperleafletCode = document.getElementById("hyperleaflet-code")
+const hyperleafletHTML = hljs.highlightAuto(
+`<div id="map"
+ data-center="39.73, 39.99"
+ data-zoom="5"
+ ...
+ >
+ <div 
+ data-tile="OpenStreetMap">
+ </div>
+ <div 
+ data-tile="EsriWorldImagery">
+ </div>
+</div>
+`).value
+
+hyperleafletCode.innerHTML = hyperleafletHTML
+
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
+
